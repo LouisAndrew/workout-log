3fea9f0bc9f1abd621ec0310f8e983bdcdf6c287
Louis Andrew <louis.andrew@share-now.com>
2021-04-08 11:47:41 +0200

    feat(auth): basic auth function

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
-import React from 'react';
-
-import { useSupabase } from '@h/useSupabase';
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '@h/useAuth';
 
 function App() {
-  const { get } = useSupabase();
+  const [r, setR] = useState(false);
+  const { getUser, signIn, signUp } = useAuth();
+
+  useEffect(() => {
+    if (r) {
+      setR(false);
+    }
+  }, [r]);
+
+  const email = 'louisandrew3@gmail.com';
+  const password = 'abcdef';
 
   return (
     <div className="App">
       <header className="App-header" />
-      <button type="button" onClick={() => console.log(get())}>
-        get supabase
-      </button>
+      {getUser() ? (
+        JSON.stringify(getUser())
+      ) : (
+        <>
+          <button
+            type="button"
+            onClick={() => signIn(email, password).then(() => setR(true))}
+          >
+            sign in
+          </button>
+          <button
+            type="button"
+            onClick={() => signUp(email, password).then(() => setR(true))}
+          >
+            sign up
+          </button>
+        </>
+      )}
     </div>
   );
 }
