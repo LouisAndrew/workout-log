417fa08da0c5855b1345ef9cc9844f7df9520464
Louis Andrew <louis.andrew@share-now.com>
2021-04-08 14:54:05 +0200

    feat(auth-hook): ability to save login cred

--- src/hooks/providers/useProvideAuth.ts
+++ src/hooks/providers/useProvideAuth.ts
@@ -1,17 +1,30 @@
+import { useEffect, useState } from 'react';
 import { User } from '@supabase/supabase-js';
-import { useState } from 'react';
 
 import { useSupabase } from '@h/useSupabase';
 
-export type AuthFunc = (email: string, password: string)
-                          => Promise<User | Error | null | undefined>;
+export type AuthFunc = (
+  email: string,
+  password: string,
+  saveUser?: boolean
+) => Promise<User | Error | null | undefined>;
+
+const localStorageId = 'saved-user';
 
 // eslint-disable-next-line import/prefer-default-export
 export const useProvideAuth = () => {
   const [user, setUser] = useState<User | null>(null);
   const { get } = useSupabase();
 
-  const signUp: AuthFunc = async (email, password) => {
+  const saveLocalUser = (u : User) => {
+    window.localStorage.setItem(localStorageId, JSON.stringify(u));
+  };
+  const loadLocalUser = (): User | null => {
+    const u = window.localStorage.getItem(localStorageId);
+    return u ? JSON.parse(u) : null;
+  };
+
+  const signUp: AuthFunc = async (email, password, saveUser = false) => {
     const supabase = get();
     if (supabase) {
       const { user: signedUpUser, error } = await supabase.auth.signUp({
@@ -19,18 +32,19 @@ export const useProvideAuth = () => {
         password,
       });
 
-      if (!error) {
+      if (!error && signedUpUser) {
         setUser(signedUpUser);
+        if (saveUser) {
+          saveLocalUser(signedUpUser);
+        }
         return signedUpUser;
       }
-
       return error;
     }
-
     return null;
   };
 
-  const signIn: AuthFunc = async (email, password) => {
+  const signIn: AuthFunc = async (email, password, saveUser = false) => {
     const supabase = get();
     if (supabase) {
       const { user: signedInUser, error } = await supabase.auth.signIn({
@@ -38,16 +52,24 @@ export const useProvideAuth = () => {
         password,
       });
 
-      if (!error) {
+      if (!error && signedInUser) {
         setUser(signedInUser);
+        if (saveUser) {
+          saveLocalUser(signedInUser);
+        }
         return signedInUser;
       }
-
       return error;
     }
-
     return null;
   };
+
+  useEffect(() => {
+    const u = loadLocalUser();
+    if (u) {
+      setUser(u);
+    }
+  }, []);
 
   return { signIn, signUp, user };
 };
