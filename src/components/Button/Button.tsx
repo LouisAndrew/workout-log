49e4b58fb9efa5ab4619a7335008a73ca39ca789
Louis Andrew <louis.andrew@share-now.com>
2021-04-14 21:26:39 +0200

    feat(button): revamp button design system

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,54 +1,72 @@
 import { IconType } from 'react-icons/lib';
 import React, { FC } from 'react';
 
-import './styles.css';
+import './styles.scss';
 
 export type Props = {
   /**
    * size of the button
    */
-  size?: 's' | 'm' | 'l'
+  size?: 's' | 'm' | 'l' | 'xs';
   /**
    * type of the button
    */
-  type?: 'primary' | 'secondary' | 'remove' | 'ghost'
+  type?: 'primary' | 'secondary' | 'remove' | 'ghost';
   /**
    * native type of the button
    */
-  nativeType?: 'button' | 'submit' | 'reset'
+  nativeType?: 'button' | 'submit' | 'reset';
   /**
    * background color of the button (see: tailwind bg convention)
    */
-  bg?: string
+  bg?: string;
   /**
    * text color of the button (see: tailwind text convention)
    */
-  color?: string
+  color?: string;
   /**
    * click handle function
    */
-  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
   /**
    * icon to render with the children
    */
-  Icon?: IconType
+  Icon?: IconType;
   /**
    * additional class name(s) for styling (tailwind)
    */
-  className?: string
+  className?: string;
   /**
    * additional props
    */
-  [key: string]: any
-}
+  [key: string]: any;
+};
 
 const Button: FC<Props> = ({
-  children, type = 'secondary', nativeType = 'button', bg, color, onClick, Icon, className, size = 'm', ...rest
+  children,
+  type = 'secondary',
+  nativeType = 'button',
+  onClick,
+  Icon,
+  className,
+  size = 'm',
+  ...rest
 }) => (
-  // eslint-disable-next-line react/button-has-type
-  <button type={nativeType} onClick={onClick} className={`button button--${type} ${size === 'm' ? '' : `button-${size}`} bg-${bg || ''} text-${color || ''} ${className}`} {...rest}>
+  <button
+    // eslint-disable-next-line react/button-has-type
+    type={nativeType}
+    onClick={onClick}
+    className={`button button--${type} ${
+      size === 'm' ? '' : `button--${size}`
+    }  ${className}`}
+    {...rest}
+  >
     {Icon && (
-      <span className="button__icon-wrapper" role="img">
+      <span
+        className="button__icon-wrapper"
+        role="img"
+        style={{ marginRight: !children ? 0 : undefined }}
+      >
         <Icon />
       </span>
     )}
