5a58e2c177ee73a52e4006d8afcdaa7bd2368fa2
Louis Andrew <louis.andrew@share-now.com>
2021-04-16 12:43:30 +0200

    feat(basic-input): dark mode support

--- src/stories/BasicInput.stories.tsx
+++ src/stories/BasicInput.stories.tsx
@@ -6,7 +6,7 @@ import PasswordInput from '@components/Input/BasicInput/PasswordInput';
 
 export default {
   title: 'Components/Input/BasicInput',
-  component: BasicInput
+  component: BasicInput,
 };
 
 const Template: Story<Props> = (args) => <BasicInput {...args} />;
@@ -15,7 +15,21 @@ export const Default = Template.bind({});
 
 export const WithError = Template.bind({});
 WithError.args = {
-  errorMsg: 'Please add this'
+  errorMsg: 'Please add this',
 };
 
-export const ForPassword: Story<Props> = () => <PasswordInput inputId="s" className="w-72" />;
+export const ForPassword: Story<Props> = () => (
+  <PasswordInput inputId="s" className="w-72" />
+);
+
+export const DarkMode = () => (
+  <div className="dark">
+    <Default inputId="1" />
+  </div>
+);
+
+export const PasswordDarkMode = () => (
+  <div className="dark">
+    <ForPassword inputId="1" />
+  </div>
+);
